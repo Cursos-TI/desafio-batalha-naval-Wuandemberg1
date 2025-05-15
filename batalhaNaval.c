d433bc8c56e16e08e0875b50f107b2e4b84f6d87
Wuandemberg1 <wuandemberg@gmail.com>
2025-05-15 21:47:55 +0000

    Completo

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,146 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+int main () {
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
-
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
+    int tabuleiro[10][10] = {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+    };
 
-    return 0;
+    char colunas[10] = {'A','B','C','D','E','F','G','H','I','J'};
+
+ 
+      for (int i = 9; i < 10; i++){
+
+         for (int j = 0; j < 3; j++) {
+        tabuleiro[i][j] = 3;  
+
+      }
+
+    }
+
+        for (int j = 0; j < 1; j++){
+
+         for (int i = 5; i < 8; i++) {
+        tabuleiro[i][j] = 3;  
+
+      }
+
+    }
+
+        for (int i, j = 3 ; i == j && i, j < 6; i++, j++){
+
+        
+        tabuleiro[i][j] = 3;  
+
+      
+
+    }
+
+    
+        for (int i = 0; i < 10; i++) {
+   
+            for (int j = 0; j < 10; j++) {
+        
+                if (i + j == 9 && i < 3) {
+           
+                    tabuleiro[i][j] = 3;
+        }
+    }
 }
+
+for (int i = 0; i < 10; i++) {
+   
+            for (int j = 0; j < 10; j++) {
+        
+                if ((i == 5) && (j >= 4 && j <= 6)) {
+           
+                    tabuleiro[i][j] = 4;
+        }
+
+            if ((j == 5) && (i >= 4 && i <= 6)) {
+
+                tabuleiro[i][j] = 4;
+            }
+    }
+}
+
+for (int i = 0; i < 10; i++) {
+   
+            for (int j = 0; j < 10; j++) {
+        
+                if ((i == 8) && (j >= 3 && j <= 7)) {
+           
+                    tabuleiro[i][j] = 5;
+        }
+
+            if ((j == 5) && (i >= 7 && i <= 9)) {
+
+                tabuleiro[i][j] = 5;
+            }
+    }
+}
+
+for (int i = 0; i < 10; i++) {
+   
+            for (int j = 0; j < 10; j++) {
+        
+                if ((i == 4) && (j >= 0 && j <= 4)) {
+           
+                    tabuleiro[i][j] = 6;
+        }
+
+                if ((i == 3) && (j >= 1 && j <= 3)) {
+
+                tabuleiro[i][j] = 6;
+            }
+
+            
+                if ((i == 2) && (j == 2)) {
+
+                tabuleiro[i][j] = 6;
+            }
+
+                
+    }
+}
+
+
+
+
+
+
+    printf("Tabuleiro Batalha Naval\n  ");
+
+    for (int j = 0; j < 10; j++)
+        printf(" %c", colunas[j]);
+
+    printf("\n");
+
+    for (int i = 0; i < 10; i++) {
+        printf("%2d", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf(" %d", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+
+    
+
+
+    
+    return 0;
+
+}
+
